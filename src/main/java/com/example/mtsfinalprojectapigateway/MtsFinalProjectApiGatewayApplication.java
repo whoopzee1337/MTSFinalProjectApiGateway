3f17583d837d987e78d659a85996de35a54a1fd4
whoopzee1337 <whoopzee228@icloud.com>
2023-05-09 17:32:09 +0300

    фыв

--- src/main/java/com/example/mtsfinalprojectapigateway/MtsFinalProjectApiGatewayApplication.java
+++ src/main/java/com/example/mtsfinalprojectapigateway/MtsFinalProjectApiGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MtsFinalProjectApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MtsFinalProjectApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MtsFinalProjectApiGatewayApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/mtsfinalprojectapigateway/MtsFinalProjectApiGatewayApplicationTests.java
+++ src/test/java/com/example/mtsfinalprojectapigateway/MtsFinalProjectApiGatewayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MtsFinalProjectApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
